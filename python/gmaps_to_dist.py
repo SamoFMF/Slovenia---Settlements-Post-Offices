ba8841c309ba3f9cc7fcdfbe5b0867f83566794d
Samo <samo.metlicar@hotmail.com>
2021-08-23 20:00:10 +0200

    Gmaps functionality

---
+++ python/gmaps_to_dist.py
@@ -0,0 +1,51 @@
+# We create 2 different networks:
+#   1. using distances from gmaps
+#   2. using times from gmaps
+from utils import loadFile, saveFile
+from datastructure import Entry
+from create_pajek_graphs import distToGraph
+from os import listdir, path
+from typing import List, Tuple
+
+def mergeResults(_path: str = "gmaps_raw") -> List[dict]:
+    result = []
+    for filename in listdir(_path):
+        result += loadFile(path.join(_path, filename))
+    print(len(result))
+    return result
+
+def createGraphs(rawData: List[dict], Graw: List[List[tuple]], n: int) -> Tuple[List[List[tuple]]]:
+    Gdist = [[] for _ in range(n)]
+    Gtime = [[] for _ in range(n)]
+    for i in range(len(rawData)): # TODO: Replace with n, when distances is complete
+        data = rawData[i]
+        if data["status"] != "OK":
+            # ERROR
+            print("ERROR in line:", i)
+            continue
+        data = data["rows"][0]["elements"]
+        valid = [j for j,_ in Graw[i] if j>i]
+        if len(data) != len(valid):
+            print("ERROR count:", i)
+        for j in range(len(valid)):
+            dataj = data[j]
+            if dataj["status"] != "OK":
+                print("ERROR in line:", i, j)
+                continue
+            u = valid[j]
+            dist = dataj["distance"]["value"]
+            dur = dataj["duration"]["value"]
+            Gdist[i].append((u,dist))
+            Gtime[i].append((u,dur))
+            Gdist[u].append((i,dist))
+            Gtime[u].append((i,dur))
+    return Gdist, Gtime
+
+if __name__ == "__main__":
+    rawData = mergeResults("gmaps_raw")
+    D = loadFile("data/distances10.pkl.gz")
+    G = distToGraph(D)
+
+    Gdist, Gtime = createGraphs(rawData, G, len(G))
+    saveFile("gmaps/gmaps_distances10.pkl.gz", Gdist)
+    saveFile("gmaps/gmaps_durations10.pkl.gz", Gtime)
---
+++ python/google_maps_api.py
@@ -0,0 +1,45 @@
+# Using google maps api (distance matrix)
+import requests
+from datastructure import Entry
+from utils import saveFile, loadFile, coordToDeg
+from typing import List
+from create_pajek_graphs import distToGraph
+
+key = ""
+with open("mykey.env", "r") as f:
+    key = f.readline().rstrip("\n")
+
+def createLink(origins: List[Entry], destinations: List[Entry], key: str) -> str:
+    link = "https://maps.googleapis.com/maps/api/distancematrix/json?origins="
+    for i in range(len(origins)):
+        lon, lat = origins[i].coordinates
+        link += f"{coordToDeg(lat)},{coordToDeg(lon)}"
+        if i+1 < len(origins):
+            link += "|"
+    link += "&destinations="
+    for i in range(len(destinations)):
+        lon, lat = destinations[i].coordinates
+        link += f"{coordToDeg(lat)},{coordToDeg(lon)}"
+        if i+1 < len(destinations):
+            link += "|"
+    link += "&key="
+    link += key
+    return link
+
+if __name__ == "__main__":
+    D = loadFile("data/distances10.pkl.gz")
+    data,_ = loadFile("data/naselja_poste.pkl.gz")
+    G = distToGraph(D)
+
+    print("START REQUESTS")
+    istart = 300
+    iend = 500
+    results = []
+    for i in range(istart, iend):
+        if i%10 == 0: print(i)
+        origins = [data[i]]
+        destinations = [data[j] for j,_ in G[i] if j>i]
+        results.append(requests.post(createLink(origins, destinations, key)).json())
+    print("SAVING FILE")
+    saveFile(f"gmaps_raw/distances_gmaps_{istart:04d}_{iend:04d}.pkl.gz", results)
+    print("DONE")
